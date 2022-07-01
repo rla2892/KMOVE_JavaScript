303d0fbf5a746477c59b386c0cfd37dbcb9fdc3c
Kim Sangjin <rla2892@naver.com>
2022-07-01 12:00:53 +0900

    string

---
+++ variablesNumbers.js
@@ -0,0 +1,9 @@
+let numberFirst = 3;
+let numberSecond = 5;
+
+console.log(numberFirst + numberSecond);
+
+// let numberThird = 7;
+numberSecond = 7;
+let result = numberFirst + numberSecond;
+console.log(result);
---
+++ variablesStrings.js
@@ -0,0 +1,35 @@
+let stringFirst = 3;
+let stringSecond = "5";
+
+
+console.log(typeof stringFirst);
+console.log(typeof stringSecond);
+
+console.log(stringFirst + stringSecond);
+console.log(stringFirst - stringSecond);
+console.log(stringFirst * stringSecond);
+
+stringSecond = "A";
+console.log(stringFirst + stringSecond);
+console.log(stringFirst - stringSecond);
+console.log(stringFirst * stringSecond);
+
+let animal = "tiger";
+console.clear();
+
+animal.charAt(0)
+'t'
+animal.charAt(1)
+'i'
+animal.charAt(8)
+''
+animal.replace("t","T")
+'Tiger'
+animal.replace("ger","gers")
+'tigers'
+animal.substring(0, 1)
+'t'
+animal.substring(0, 2)
+'ti'
+animal.substring(1, 2)
+'i'
