05aa20a71b03ce2cd41830de423742d5a918a781
Kim Sangjin <rla2892@naver.com>
2022-07-04 12:55:02 +0900

    array

---
+++ arrays.js
@@ -0,0 +1,14 @@
+let fruits = ["appple", "banana", "orange"];
+// fruits[0, 1, 2 ...]
+fruits.push("grape");
+fruits.pop();
+
+console.log()
+
+
+let animals = ["tiger", "lion"];
+
+let mix = ["appple", 1, "banana", ["tiger", "lion"],"orange"];
+
+console.log("end");
+
--- loops.js
+++ loops.js
@@ -2,13 +2,37 @@ let count = 0;
 
 // while(true){
 // let condition = count < 100;
-while(count < 100) {
+while(count < 10) {
     count = count + 1;
     console.log('in while ' + count);
 }
 
 console.log("end");
 
-for(let i = 0; i < 100; i++) {
+for(let i = 0; i < 10; i++) {
     console.log("in for with count : "+ i);
+}
+
+let fruits = ["appple", "banana", "orange"];
+for(let index=0; index<2; index++){
+    console.log("fr : " + fruits[index]);
+}
+
+let mix = ["appple", 1, "banana", ["tiger", "lion"],"orange"];
+for(let item of mix){
+    console.log(item);
+
+    if( typeof item != 'string' 
+    && typeof item != 'number'  
+    && typeof item != 'boolean'){
+        for(let anim in item){
+            console.log(item[anim]);
+        }
+    }
+
+}
+
+let animals = ["tiger", "lion"];
+for(let anim of animals){
+    console.log(anim);
 }
