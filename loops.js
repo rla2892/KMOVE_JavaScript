9f6bdd841ab6077235f56eca07d76383d022dd74
Kim Sangjin <rla2892@naver.com>
2022-07-04 10:36:42 +0900

    loop

---
+++ loops.js
@@ -0,0 +1,14 @@
+let count = 0;
+
+// while(true){
+// let condition = count < 100;
+while(count < 100) {
+    count = count + 1;
+    console.log('in while ' + count);
+}
+
+console.log("end");
+
+for(let i = 0; i < 100; i++) {
+    console.log("in for with count : "+ i);
+}
